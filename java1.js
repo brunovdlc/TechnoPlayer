3baa2bc7ea95f5f95878bf2f1ea9aeb62f3d7669
BrunoT <brunovdlc@hotmail.com>
2023-05-29 16:40:08 -0300

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,10 @@
+ var typing = new Typed (".animate",{
+      strings:['xat.com/TechnoPlayer','Servidores de radio de alta calidad','servicio y precio y atencion personal'],
+      typeSpeed:120,
+      backSpeed:70,
+      loop:true,
+      
+   });
+
+
+ 
